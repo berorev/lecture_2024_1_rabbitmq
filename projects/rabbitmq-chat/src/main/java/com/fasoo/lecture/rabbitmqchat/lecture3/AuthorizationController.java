e0aa88840db26fcb08a7e8257f7702311039e4d2
berorev <berorev@gmail.com>
2024-04-11 16:58:25 +0900

    ..

---
+++ projects/rabbitmq-chat/src/main/java/com/fasoo/lecture/rabbitmqchat/lecture3/AuthorizationController.java
@@ -0,0 +1,9 @@
+package com.fasoo.lecture.rabbitmqchat.lecture3;
+
+@RestController
+@RequestMapping("/user")
+public class AuthorizationController {
+  public String test() {
+    return "test";
+  }
+}
